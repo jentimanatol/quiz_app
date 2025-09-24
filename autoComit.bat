78722443643c35e59cfa7f3b4976a12e894c03e3
Anatolie Jentimir <157539682+jentimanatol@users.noreply.github.com>
2025-09-24 19:30:15 -0400

    'Auto Commit'

--- autoComit.bat
+++ autoComit.bat
@@ -4,6 +4,6 @@ git commit -m "'Auto Commit"'"
 git push origin main
 
 :: === Tagging for GitHub Actions Release Build ===
-git tag v1.0.3
-git push origin v1.0.3
+git tag v1.0.4
+git push origin v1.0.4
 pause
